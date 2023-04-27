7d485e7526ed48e78e224b270c7402b1e9378894
daoxuandong <90700841+daoxuandong@users.noreply.github.com>
2023-04-27 09:54:22 +0700

    lop1

--- assets/js/khoa.js
+++ assets/js/khoa.js
@@ -4,24 +4,21 @@ const phanlop =document.querySelector('.js-phanlop')
 const phanlopContainer = document.querySelector('.js-phanlop-container')
 const phanlopClose = document.querySelector('.js-phanlop-close')
 //hàm hiển thị phanlop mua vé
-function showBuyTickets(){
+function showlop(){
     phanlop.classList.add('open')
 }
 //hàm ẩn phanlop mua vé(gỡ bỏ class)
-function hideBuyTickets(){
+function hidelop(){
     phanlop.classList.remove('open')
 }
 //lặp qua từng thẻ button và nghe hành vi click
 for(const buyBtn of buyBtns){
-    buyBtn.addEventListener('click', showBuyTickets)
+    buyBtn.addEventListener('click', showlop)
 }
 //nghe hành vi click vào button
-phanlopClose.addEventListener('click', hideBuyTickets)
+phanlopClose.addEventListener('click', hidelop)
 
-phanlop.addEventListener('click',hideBuyTickets)
+phanlop.addEventListener('click',hidelop)
 phanlopContainer.addEventListener('click', function (event) {
     event.stopPropagation();
 })
-
-
-
